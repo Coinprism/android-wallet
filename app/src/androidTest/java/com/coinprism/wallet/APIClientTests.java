7b8ad6533cc2fd2ab63b34fcc15599653e8a2649
Flavien Charlon <flavien@charlon.net>
2015-02-03 16:30:56 -0800

    Add unit tests for the Formatting class

--- app/src/androidTest/java/com/coinprism/wallet/APIClientTests.java
+++ app/src/androidTest/java/com/coinprism/wallet/APIClientTests.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2014 Flavien Charlon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.coinprism.wallet;
 
 import android.test.InstrumentationTestCase;
@@ -15,7 +32,7 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.util.List;
 
-public class APIClient extends InstrumentationTestCase
+public class APIClientTests extends InstrumentationTestCase
 {
     public void test_getAddressBalance_noAsset() throws Exception
     {
